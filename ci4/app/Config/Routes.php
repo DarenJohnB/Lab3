994e07a21848678699b8a3d5923a89ce0a43ada8
DarenJ <dbbarcia2@student.apc.edu.ph>
2024-06-23 19:58:05 +0800

    Commits and Push

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -7,7 +7,14 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+use App\Controllers\Articles; // Add this line
+use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
+
+$routes->resource('articles'); // Add this line
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
